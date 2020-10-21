f899ae20509c925c74b69cd0fed4ad85482d883a
kpegeder <k.pegeder@gmail.com>
2020-10-20 17:50:16 -0700

    add routes for base HTML and update movieSearch.js

--- public/js/movieSearch.js
+++ public/js/movieSearch.js
@@ -1,46 +1,51 @@
-function displayMovieInfo() {
-  // var movie = $(this).attr("data-name");
-  let movie = $("#movie-input").val().trim();
-  const queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+$(document).ready(function () {
+  function displayMovieInfo() {
+    // event.preventDefault();
+    // var movie = $(this).attr("data-name");
+    let movie = $("#movie-input").val().trim();
+    const queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-  // Create holding variable
-  // let results;
+    // Create holding variable
+    // let results;
 
-  // Creating an AJAX call for the specific movie button being clicked
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (res) {
-    console.log(res);
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (res) {
+      console.log(res);
 
-    let addMovie = {
-      title: res.Title,
-      poster: res.Poster,
-      genre: res.Genre,
-      actor: res.Actors,
-      runtime: res.Runtime,
-      metaCritic: res.Metascore,
-      imdb: res.imdbRating,
-    };
+      let addMovie = {
+        title: res.Title,
+        poster: res.Poster,
+        genre: res.Genre,
+        actor: res.Actors,
+        plot: res.Plot,
+        runtime: res.Runtime,
+        metaCritic: res.Metascore,
+        imdb: res.imdbRating,
+      };
 
-    submitMovie(addMovie);
-  });
-}
+      submitMovie(addMovie);
+    });
+  }
 
-function submitMovie(movie) {
-  $.post("/api/movie/", movie);
-}
-// $("#add-movie").on("click", function (event) {
-//   event.preventDefault();
-//   // This line grabs the input from the textbox
-//   let movie = $("#movie-input").val().trim();
-//   console.log(movie);
+  function submitMovie(movie) {
+    $.post("/api/movie/", movie);
+  }
+  $("#add-movie").on("click", displayMovieInfo);
 
-//   // // Adding movie from the textbox to our array
-//   // movies.push(movie);
-// });
+  // function (event) {
+  //   event.preventDefault();
+  //   // This line grabs the input from the textbox
+  //   let movie = $("#movie-input").val().trim();
+  //   console.log(movie);
 
-$(document).on("click", "#add-movie", displayMovieInfo);
+  // // Adding movie from the textbox to our array
+  // movies.push(movie);
+  // });
+});
+// $(document).on("click", "#add-movie", displayMovieInfo);
 
 // $(document).ready(function () {
 //   function getMovieInto() {}
