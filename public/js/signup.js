d1073fbab1d932c0fb635665c0ea4ebbd1c4836e
kpegeder <k.pegeder@gmail.com>
2020-10-23 15:48:12 -0700

    create user and verify accounts

---
+++ public/js/signup.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  let username = $("#username");
+  let email = $("#email");
+  let password = $("#password");
+
+  $("#signup").on("click", handleUserInfo);
+
+  function handleUserInfo(event) {
+    event.preventDefault();
+
+    let user = {
+      username: username.val().trim(),
+      email: email.val().trim(),
+      password: password.val().trim(),
+    };
+    if (!user.username || !user.email || !user.password) {
+      return;
+    }
+
+    createUser(user);
+
+    username.val("");
+    email.val("");
+    password.val("");
+  }
+
+  function createUser(user) {
+    $.post("/api/signup", user)
+      .then(function (data) {
+        window.location.href("/homepage");
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // Dependencies
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 
-// Set up the Express App
-const app = express();
+// Require passport from config
+const passport = require("./config/passport");
+
+// Set up the Port
 const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 const db = require("./models");
 
 // Set up the Express app to handle data parsing
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,13 +21,18 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
-
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "profile" }));
 app.set("view engine", "handlebars");
 
+// Session to track user login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Import routes and give the server access to them
-// Need to set up routes
 const routesHTML = require("./routes/html-routes");
 const routesMovie = require("./routes/movies_db-routes");
 const routesAccount = require("./routes/account-api-routes");
