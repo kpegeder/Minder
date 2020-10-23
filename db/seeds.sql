a42cb6b14da71b4fa4f2ff7941dbea56f6606ea3
kpegeder <k.pegeder@gmail.com>
2020-10-22 22:06:45 -0700

    update signup page

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE movies_db;
+
+insert into Movies (title, createdAt, updatedAt)
+values ("Honest Thief", "01/01/01","01/01/01"),
+("Tenet", "01/01/01","01/01/01"),
+("The Empty Man", "01/01/01","01/01/01"),
+("Halloween", "01/01/01","01/01/01"),
+("Scream", "01/01/01","01/01/01"),
+("Unhinged", "01/01/01","01/01/01"),
+("Goosebumps", "01/01/01","01/01/01"),
+("Possessor", "01/01/01","01/01/01"),
+("It", "01/01/01","01/01/01"),
+("Annabelle", "01/01/01","01/01/01"),
+("The Nest", "01/01/01","01/01/01"),
+("The Conjuring", "01/01/01","01/01/01");
